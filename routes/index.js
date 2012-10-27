073a74e45798d2fdd3fa8a99471056ab1daf0652
Alex Grigoroi <alex@grigoroi.com>
2012-10-28 00:29:54 +0100

    the search now displays a map centered at the city searched

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,34 @@
  * GET home page.
  */
 
+var http = require("http");
+
+gencity = function(city){
+  var options = {
+    host:"maps.googleapis.com",
+    port:80,
+    path:"/maps/api/geocode/json?address="+city+"&sensor=false"
+  }
+  return options;
+}
+
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
 exports.search = function(req, res){
-  res.render('search', {title: 'Search', city: req.param('city', null)})
+  http.get(gencity(req.param('city', null)), function(googleres){
+    var pagedata="";
+    googleres.setEncoding("utf-8");
+    googleres.on('data', function(chunk){
+      pagedata += chunk;
+    });
+
+    googleres.on('end', function(){
+      result = JSON.parse(pagedata);
+      var location = result.results[0].geometry.location;
+      res.render('search', {title: 'Search', lat: location.lat, lng: location.lng});
+    });
+  });
 }
+
