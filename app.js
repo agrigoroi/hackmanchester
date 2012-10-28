9ac6f6638be9f7729712442c932140f2bbc3fc43
Alex Grigoroi <alex@grigoroi.com>
2012-10-28 04:14:43 +0000

    Random cities in the header

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.post('/search', routes.search);
+app.get('/search', routes.search);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,19 @@
  */
 
 var http = require("http");
+var numberOfCityInHeader = 12;
+
+getRandomCities = function(number)
+{
+  cities =["Istanbul", "Moscow", "London", "Saint Petersburg", "Berlin", "Madrid", "Kiev", "Rome", "Paris", "Bucharest", "Hamburg", "Minsk", "Warsaw", "Vienna", "Barcelona", "Budapest", "Kharkiv", "Munich", "Milan", "Stockholm", "Nizhniy Novgorod", "Prague", "Sofia", "Kazan", "Samara", "Belgrade", "Copenhagen", "Dublin", "Brussels", "Rostov-na-Donu", "Dnipropetrovsk", "Ufa", "Cologne", "Odesa", "Donetsk" , "Perm", "Volgograd", "Naples", "Birmingham", "Valencia", "Oslo", "Turin", "Voronezh", "Saratov", "Zaporizhzhya", "Marseille", "Amsterdam", "Krakow", "Athens", "Lodz"];
+  res = [];
+  for(var i=0;i<number;i++)
+    res.push(cities[Math.floor(Math.random()*50)]);
+  return res;
+}
+
 
 gencity = function(city){
-  
   var options = {
     host:"maps.googleapis.com",
     port:80,
@@ -16,11 +26,17 @@ gencity = function(city){
 }
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express',  background:true, error:false });
+  res.render('index', { title: 'Express',  background:true, error:false, cities: getRandomCities(numberOfCityInHeader) });
 };
 
 exports.search = function(req, res){
-  http.get(gencity(req.param('city', null)), function(googleres){
+  var city;
+  if(typeof(req.query["city"]) !== "Undefined")
+    city = req.query["city"];
+  else
+    city = req.param('city', null);
+
+  http.get(gencity(city), function(googleres){
     var pagedata="";
     googleres.setEncoding("utf-8");
     googleres.on('data', function(chunk){
@@ -30,10 +46,10 @@ exports.search = function(req, res){
     googleres.on('end', function(){
       result = JSON.parse(pagedata);
       if (result.status=="ZERO_RESULTS")
-        res.render('index', { title: 'Express',  background:true , error:true});
+        res.render('index', { title: 'Express',  background:true , error:true, cities: getRandomCities(numberOfCityInHeader)});
       else{
         var location = result.results[0].geometry.location;
-        res.render('search', {title: 'Search', lat: location.lat, lng: location.lng, background:false});
+        res.render('search', {title: 'Search', lat: location.lat, lng: location.lng, background:false, cities: getRandomCities(numberOfCityInHeader)});
       }
     });
   });
